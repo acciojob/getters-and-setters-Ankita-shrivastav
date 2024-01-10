82efd5b3dfadd5af968546a3f4fa9c81daf4b992
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2024-01-11 00:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this._name=name;
-		this._age=age;
+		this.name=name;
+		this.age=age;
 	}
 	get name(){
 		return this._name;
+	}
+	set name(name){
+		this._name=name;
+	}
+	get age(){
+		return this._age;
 	}
 	set age(age){
 		this._age=age;
@@ -14,13 +20,13 @@ class Person {
 
 class Student extends Person {
 	study(){
-		console.log(`${this._name}is studying.`);
+		console.log(`${this.name}is studying.`);
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${this._name} is teaching.`);
+		console.log(`${this.name} is teaching.`);
 	}
 }
 
